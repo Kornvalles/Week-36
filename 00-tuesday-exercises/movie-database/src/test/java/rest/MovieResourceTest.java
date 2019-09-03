1156d045b6140645c34bfb116c995b5fb053f171
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-03 11:02:05 +0200

    path updated

--- 00-tuesday-exercises/movie-database/src/test/java/rest/MovieResourceTest.java
+++ 00-tuesday-exercises/movie-database/src/test/java/rest/MovieResourceTest.java
@@ -86,7 +86,7 @@ public class MovieResourceTest {
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/xxx").then().statusCode(200);
+        given().when().get("/movie").then().statusCode(200);
     }
    
     //This test assumes the database contains two rows
@@ -94,7 +94,7 @@ public class MovieResourceTest {
     public void testDummyMsg() throws Exception {
         given()
         .contentType("application/json")
-        .get("/xxx/").then()
+        .get("/movie/").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
         .body("msg", equalTo("Hello World"));   
@@ -104,7 +104,7 @@ public class MovieResourceTest {
     public void testCount() throws Exception {
         given()
         .contentType("application/json")
-        .get("/xxx/count").then()
+        .get("/movie/count").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
         .body("count", equalTo(2));   
