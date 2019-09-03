c9c165c4c5210c2f19c73e79fb60a0f5bde85992
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-03 10:34:32 +0200

    path update

--- 00-tuesday-exercises/movie-database/src/main/java/rest/MovieDatabaseResource.java
+++ 00-tuesday-exercises/movie-database/src/main/java/rest/MovieDatabaseResource.java
@@ -16,7 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
+@Path("api")
 public class MovieDatabaseResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
