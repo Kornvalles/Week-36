17f8c61eeb93a38e59e19d5291e69c36e2aac1f2
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-03 10:17:29 +0200

    database changed

--- 00-tuesday-exercises/movie-database/target/generated-sources/annotations/entities/Movie_.java
+++ 00-tuesday-exercises/movie-database/target/generated-sources/annotations/entities/Movie_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-03T10:07:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-03T10:16:43")
 @StaticMetamodel(Movie.class)
 public class Movie_ { 
 
