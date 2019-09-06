042f2ca5f7372fed99c106edb6744150ba95267e
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-07 01:50:05 +0200

    Clean up

--- 02-wednesday-exercises/js_day1/src/theJsArray.js
+++ 02-wednesday-exercises/js_day1/src/theJsArray.js
@@ -22,53 +22,53 @@ var girls = ["Janne", "hanne", "Sanne"];
 
 //  Exercise B - works.
 var all = boys.concat(girls);
-//  console.log(all);
+  console.log(all);
 
 
 //  Exercise C - works.
-//  console.log(all.join('-'));
-//  console.log(all.join(','));
+  console.log(all.join('-'));
+  console.log(all.join(','));
 
 
 //  Exercise D & E - works.
-//  console.log(all.push("Lone","Gitte"));
-//  console.log(all.unshift("Hans","Kurt"));
-//  console.log(all);
+  console.log(all.push("Lone","Gitte"));
+  console.log(all.unshift("Hans","Kurt"));
+  console.log(all);
 
 
 //  Exercise F & G - works.
-//  console.log(all.shift("Hans"));
-//  console.log(all.pop("Gitte"));
-//  console.log(all);
+  console.log(all.shift("Hans"));
+  console.log(all.pop("Gitte"));
+  console.log(all);
 
 
 //  Exercise H - works.
-//  console.log(all.splice(2, 2));
-//  console.log(all);
+  console.log(all.splice(2, 2));
+  console.log(all);
 
 
 //  Exercise I - works.
-//  console.log(all.reverse());
+  console.log(all.reverse());
 
 
 //  Exercise J & K - works.
-//  console.log(all.sort());
-//  var sorted = all.map(function (value) {
-//  return value.toLowerCase();
-//  }).sort();
-//  console.log(sorted);
+  console.log(all.sort());
+  var sorted = all.map(function (value) {
+  return value.toLowerCase();
+  }).sort();
+  console.log(sorted);
 
 
 //  Exercise L - works.
-//  var sorted = all.map(function (value) {
-//  value = value.toUpperCase();
-//  return value;
-//  });
-//  console.log(sorted);
+  var sorted = all.map(function (value) {
+  value = value.toUpperCase();
+  return value;
+  });
+  console.log(sorted);
 
 
 //  Exercise M
-// let filtered = all.filter((name === name.startsWith("L)" )))
+ let filtered = all.filter((name === name.startsWith("L)" )))
 
 
 
